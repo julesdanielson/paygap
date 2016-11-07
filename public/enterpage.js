7206da8f6e22644bfac72c940034db1e17c63812
Julianna Sophia <juliannasophia@150-253-91-61.wifi.mcad.edu>
2016-11-07 14:28:09 -0600

    took out enterpage

--- public/enterpage.js
+++
@@ -1,5 +0,0 @@
-var socket = io.connect() //connect to the server
-
-socket.on('connect', function(data){ // when connected, do something
-  console.log("connected to the server" + socket.id); // log out our id
-})
